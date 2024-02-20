ab11f51bf3714287bfbdbaf5f04dcc4ad3bc73c4
Somewhere <likeaboss13371@gmail.com>
2024-02-20 23:11:24 +0200

    Task Details View And Views Names Added

---
+++ TrelloApp/Views/TaskDetails.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TrelloApp.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskDetails.xaml
+    /// </summary>
+    public partial class TaskDetails : Window
+    {
+        public TaskDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
