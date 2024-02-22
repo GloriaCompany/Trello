c9a8d14e5af838db52ebd5ad7ff0a12cbcb1154b
Yar <41117254+Nerwe@users.noreply.github.com>
2024-02-22 17:40:55 +0200

    Fix in TaskModel
    Small typo

--- TrelloApp/Models/TaskModel.cs
+++ TrelloApp/Models/TaskModel.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 namespace TrelloApp.Models
 {
     public class TaskModel
-    {
+    { 
         public int TaskID { get; set; }
         public UserModel UserID { get; set; } = new UserModel();
         public string Title { get; set; }
         public string Description { get; set; }
-        public List<CheckBoxModel> Boards { get; set; } = new List<CheckBoxModel>();
+        public List<CheckBoxModel> CheckBoxes { get; set; } = new List<CheckBoxModel>();
     }
 }
