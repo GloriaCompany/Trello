7534b489188181366c010e98df49d9c8806f34e2
Артем <artemus200918@gmail.com>
2024-02-22 16:06:24 +0200

    fix Dushboard

--- TrelloApp/Models/DashboardModel.cs
+++ TrelloApp/Models/DashboardModel.cs
@@ -6,13 +6,14 @@ namespace TrelloApp.Models
     public class DashboardModel
     {
         public string Title { get; set; }
-        public List<UserModel> Users { get; set; }
+        public string UserName { get; set; }
+        public string UserAvatar { get; set; }
         public List<TaskDetailesModel> Tasks { get; set; }
 
         public DashboardModel()
         {
             Tasks = new List<TaskDetailesModel>();
-            Users = new List<UserModel>();
+            
         }
     }
 }
