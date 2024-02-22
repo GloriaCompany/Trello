971fa9ab3d0bc16875b00790705a2dfacbcc9621
Артем <artemus200918@gmail.com>
2024-02-22 15:39:57 +0200

    Dashboard correct model
    add users and company to the model

--- TrelloApp/Models/DashboardModel.cs
+++ TrelloApp/Models/DashboardModel.cs
@@ -6,11 +6,15 @@ namespace TrelloApp.Models
     public class DashboardModel
     {
         public string Title { get; set; }
+        public string CompanyName { get; set; }
+        public int CompanyAvatar { get; set; }
+        public List<UserModel> Users { get; set; }
         public List<TaskDetailesModel> Tasks { get; set; }
 
         public DashboardModel()
         {
             Tasks = new List<TaskDetailesModel>();
+            Users = new List<UserModel>();
         }
     }
 }
