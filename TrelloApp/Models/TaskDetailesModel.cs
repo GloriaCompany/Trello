64d76ed4c01564902a652a1c497bd8ffd66937ba
Артем <artemus200918@gmail.com>
2024-02-22 15:29:32 +0200

    correct model

--- TrelloApp/Models/TaskDetailesModel.cs
+++ TrelloApp/Models/TaskDetailesModel.cs
@@ -8,5 +8,13 @@ namespace TrelloApp.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public bool IsCompleted { get; set; }
+        public UserModel AssignedUser { get; set; } // Інформація про призначеного користувача
+
+
+        public TaskDetailesModel()
+        {
+            
+            AssignedUser = new UserModel();
+        }
     }
 }
--- TrelloApp/Models/UserModel.cs
+++ TrelloApp/Models/UserModel.cs
@@ -4,6 +4,7 @@ namespace TrelloApp.Models
 {
     public class UserModel
     {
+        public int Id { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
