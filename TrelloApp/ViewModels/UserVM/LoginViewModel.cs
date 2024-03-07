77ec20eb5edc64e483b3f0db316aebe2d43ddf2b
Артем <artemus200918@gmail.com>
2024-03-07 21:58:53 +0200

    Update Login.xmal and update LoginViewModel

--- TrelloApp/ViewModels/UserVM/LoginViewModel.cs
+++ TrelloApp/ViewModels/UserVM/LoginViewModel.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Configuration;
-using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using TrelloApp.Models;
-using TrelloDBLayer;
 using TrelloApp.ViewModels.Base;
-using TrelloApp.ViewModels.UserVM;
+using TrelloDBLayer;
 
 namespace TrelloApp.ViewModels.UserVM
 {
-    public class LoginViewModel : INotifyPropertyChanged
+    public class LoginViewModel : ViewModelBase
     {
-        private readonly UserModel user;
-        private readonly IUserRepository userRepository;
+        private UserModel _user;
+        public UserModel User
+        {
+            get { return _user; }
+            set
+            {
+                _user = value;
+                OnPropertyChanged(nameof(User));
+            }
+        }
+
+        private IUserRepository _userRepository;
+        public IUserRepository UserRepository { get { return _userRepository; } set { _userRepository = value; } }
 
         public event EventHandler SuccessfullyLoggedIn;
 
@@ -22,26 +30,25 @@ namespace TrelloApp.ViewModels.UserVM
 
         public LoginViewModel()
         {
-            user = new UserModel();
-            userRepository = new UserRepository();
-
+            _user = new UserModel();
             LoginCommand = new RelayCommand(Login, CanLogin);
         }
 
         private bool CanLogin()
         {
             return
-                !string.IsNullOrEmpty(user.Username) &&
-                !string.IsNullOrEmpty(user.Password);
+                User != null &&
+                !string.IsNullOrEmpty(User.Username) &&
+                !string.IsNullOrEmpty(User.Password);
         }
 
         private void Login()
         {
             try
             {
-                var existingUser = userRepository.GetUserByID(user.UserID);
+                var existingUser = _userRepository.GetUserByID(User.UserID);
 
-                if (existingUser != null && existingUser.Username == user.Username && existingUser.Password == user.Password)
+                if (existingUser != null && existingUser.Username == User.Username && existingUser.Password == User.Password)
                 {
                     SuccessfullyLoggedIn?.Invoke(this, EventArgs.Empty);
                 }
@@ -54,24 +61,6 @@ namespace TrelloApp.ViewModels.UserVM
             {
                 MessageBox.Show($"Помилка: {ex.Message}");
             }
-        }
-
-        public string Username
-        {
-            get { return user.Username; }
-            set { user.Username = value; OnPropertyChanged(nameof(Username)); }
-        }
-        public string Password
-        {
-            get { return user.Password; }
-            set { user.Password = value; OnPropertyChanged(nameof(Password)); }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
