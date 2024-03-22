66bb7b4bfb7ad39ece6a610dea6f77a2d83190dc
Somewhere <likeaboss13371@gmail.com>
2024-03-22 20:21:31 +0200

    Navigator Background FIxed

--- TrelloApp/Views/MainWindow.xaml.cs
+++ TrelloApp/Views/MainWindow.xaml.cs
@@ -5,7 +5,6 @@ using System.Windows;
 using System.Windows.Controls;
 using TrelloApp.Models;
 using TrelloApp.Views.CustomControls;
-using TrelloApp.Views.ResourcesTrello;
 
 namespace TrelloApp
 {
@@ -32,65 +31,51 @@ namespace TrelloApp
 
         private void ThemesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //// Отримуємо обрану тему в комбобоксі
-            //var selectedItem = (sender as CustomComboBox)?.SelectedItem as CustomComboBoxItem;
-
-            //// Отримуємо поточні ресурси додатку
-            //var resources = Application.Current.Resources;
-
-            //// Задаємо завантаження ресурсів в залежності від обраного елемента
-            //if (selectedItem != null && selectedItem.Content.ToString() == "Light")
-            //{
-            //    // Завантажуємо світлу тему
-            //    resources.MergedDictionaries.Add(new ResourceDictionary 
-            //    { 
-            //        Source = new Uri("/Views/ResourcesTrello/Themes/LightTheme.xaml", 
-            //        UriKind.Relative) 
-            //    });
-            //}
-            //else if (selectedItem != null && selectedItem.Content.ToString() == "Dark")
-            //{
-            //    // Завантажуємо темну тему
-            //    resources.MergedDictionaries.Add(new ResourceDictionary 
-            //    { 
-            //        Source = new Uri("/Views/ResourcesTrello/Themes/DarkTheme.xaml", 
-            //        UriKind.Relative) 
-            //    });
-            //}
-
+            // Отримуємо обрану тему в комбобоксі
             var selectedItem = (sender as CustomComboBox)?.SelectedItem as CustomComboBoxItem;
-            if (selectedItem != null)
+
+            // Отримуємо поточні ресурси додатку
+            var resources = Application.Current.Resources;
+
+            // Задаємо завантаження ресурсів в залежності від обраного елемента
+            if (selectedItem != null && selectedItem.Content.ToString() == "Light")
             {
-                var themeName = selectedItem.Content.ToString();
-                ThemeManager.ChangeTheme(themeName);
+                // Завантажуємо світлу тему
+                resources.MergedDictionaries.Add(new ResourceDictionary 
+                { 
+                    Source = new Uri("/Views/ResourcesTrello/Themes/LightTheme.xaml", 
+                    UriKind.Relative) 
+                });
+            }
+            else if (selectedItem != null && selectedItem.Content.ToString() == "Dark")
+            {
+                // Завантажуємо темну тему
+                resources.MergedDictionaries.Add(new ResourceDictionary 
+                { 
+                    Source = new Uri("/Views/ResourcesTrello/Themes/DarkTheme.xaml", 
+                    UriKind.Relative) 
+                });
             }
         }
 
         private void LanguagesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //var selectedItem = (sender as CustomComboBox)?.SelectedItem as CustomComboBoxItem;
-            //if (selectedItem != null)
-            //{
-            //    var cultureName = selectedItem.Content.ToString();
-            //    var culture = new CultureInfo(cultureName == "UA" ? "uk-UA" : "en-US");
-
-            //    // Форматування шляху до ресурсів з мовами
-            //    var languageResourcePath = $"/TrelloApp;component/Views/ResourcesTrello/Languages/{cultureName}.xaml";
-
-            //    // Завантаження ресурсів мови
-            //    var languageResource = new ResourceDictionary 
-            //    { 
-            //        Source = new Uri(languageResourcePath, 
-            //        UriKind.RelativeOrAbsolute) 
-            //    };
-            //    Application.Current.Resources.MergedDictionaries.Add(languageResource);
-            //}
-
             var selectedItem = (sender as CustomComboBox)?.SelectedItem as CustomComboBoxItem;
             if (selectedItem != null)
             {
-                var languageCode = selectedItem.Content.ToString();
-                ThemeManager.ChangeLanguage(languageCode);
+                var cultureName = selectedItem.Content.ToString();
+                var culture = new CultureInfo(cultureName == "UA" ? "uk-UA" : "en-US");
+
+                // Форматування шляху до ресурсів з мовами
+                var languageResourcePath = $"/TrelloApp;component/Views/ResourcesTrello/Languages/{cultureName}.xaml";
+
+                // Завантаження ресурсів мови
+                var languageResource = new ResourceDictionary 
+                { 
+                    Source = new Uri(languageResourcePath, 
+                    UriKind.RelativeOrAbsolute) 
+                };
+                Application.Current.Resources.MergedDictionaries.Add(languageResource);
             }
         }
     }
