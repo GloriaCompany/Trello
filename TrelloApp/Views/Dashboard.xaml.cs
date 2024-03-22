645bfa0543528d0400a033cb9f9c243d11ea6053
Somewhere <likeaboss13371@gmail.com>
2024-03-22 21:22:47 +0200

    Themes Changing Util Added

--- TrelloApp/Views/Dashboard.xaml.cs
+++ TrelloApp/Views/Dashboard.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System.Globalization;
-using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using TrelloApp.Views.CustomControls;
-using System.Linq;
+using TrelloApp.Views.Utils;
 
 namespace TrelloApp.Views
 {
@@ -62,61 +59,12 @@ namespace TrelloApp.Views
 
         private void ThemesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var selectedItem = (sender as CustomComboBox)?.SelectedItem as CustomComboBoxItem;
-            if (selectedItem != null)
-            {
-                var resources = Application.Current.Resources;
-                // Видаляємо попередні ресурси теми
-                var themeResources = resources.MergedDictionaries.Where(d => d.Source != null && d.Source.ToString().Contains("/Views/ResourcesTrello/Themes/")).ToList();
-                foreach (var themeResource in themeResources)
-                {
-                    resources.MergedDictionaries.Remove(themeResource);
-                }
-
-                var themeName = selectedItem.Content.ToString();
-                if (themeName == "Light" || themeName == "Світла")
-                {
-                    resources.MergedDictionaries.Add(new ResourceDictionary
-                    {
-                        Source = new Uri("/Views/ResourcesTrello/Themes/LightTheme.xaml", UriKind.Relative)
-                    });
-                }
-                else if (themeName == "Dark" || themeName == "Темна")
-                {
-                    resources.MergedDictionaries.Add(new ResourceDictionary
-                    {
-                        Source = new Uri("/Views/ResourcesTrello/Themes/DarkTheme.xaml", UriKind.Relative)
-                    });
-                }
-            }
+            ComboBoxUtils.HandleSelectionChanged(sender, e, "/Views/ResourcesTrello/Themes/", null);
         }
 
         private void LanguagesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var selectedItem = (sender as CustomComboBox)?.SelectedItem as CustomComboBoxItem;
-            if (selectedItem != null)
-            {
-                var resources = Application.Current.Resources;
-                // Видаляємо попередні ресурси мови
-                var languageResources = resources.MergedDictionaries.Where(d => d.Source != null && d.Source.ToString().Contains("/Views/ResourcesTrello/Languages/")).ToList();
-                foreach (var resource in languageResources)
-                {
-                    resources.MergedDictionaries.Remove(resource);
-                }
-
-                var cultureName = selectedItem.Content.ToString();
-                var culture = new CultureInfo(cultureName == "UA" ? "uk-UA" : "en-US");
-
-                // Форматування шляху до ресурсів з мовами
-                var languageResourcePath = $"/TrelloApp;component/Views/ResourcesTrello/Languages/{cultureName}.xaml";
-
-                // Завантаження ресурсів мови
-                var languageResource = new ResourceDictionary
-                {
-                    Source = new Uri(languageResourcePath, UriKind.RelativeOrAbsolute)
-                };
-                resources.MergedDictionaries.Add(languageResource);
-            }
+            ComboBoxUtils.HandleSelectionChanged(sender, e, null, "/Views/ResourcesTrello/Languages/");
         }
     }
 }
---
+++ TrelloApp/Views/Utils/ComboBoxUtils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using TrelloApp.Views.CustomControls;
+using System.Linq;
+
+namespace TrelloApp.Views.Utils
+{
+    public static class ComboBoxUtils
+    {
+        public static void HandleSelectionChanged(object sender, SelectionChangedEventArgs e, string themesResourcePath, string languagesResourcePath)
+        {
+            var selectedItem = (sender as CustomComboBox)?.SelectedItem as CustomComboBoxItem;
+            if (selectedItem != null)
+            {
+                var resources = Application.Current.Resources;
+
+                if (!string.IsNullOrEmpty(themesResourcePath) || !string.IsNullOrEmpty(languagesResourcePath))
+                {
+                    var themeResources = resources.MergedDictionaries
+                        .Where(d => d.Source != null &&
+                                    (themesResourcePath != null && d.Source.ToString().Contains(themesResourcePath)) ||
+                                    (languagesResourcePath != null && d.Source.ToString().Contains(languagesResourcePath)))
+                        .ToList();
+                    foreach (var themeResource in themeResources)
+                    {
+                        resources.MergedDictionaries.Remove(themeResource);
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(themesResourcePath))
+                {
+                    var themeName = selectedItem.Content.ToString();
+                    if (themeName == "Light" || themeName == "Світла")
+                    {
+                        resources.MergedDictionaries.Add(new ResourceDictionary
+                        {
+                            Source = new Uri(themesResourcePath + "LightTheme.xaml", UriKind.Relative)
+                        });
+                    }
+                    else if (themeName == "Dark" || themeName == "Темна")
+                    {
+                        resources.MergedDictionaries.Add(new ResourceDictionary
+                        {
+                            Source = new Uri(themesResourcePath + "DarkTheme.xaml", UriKind.Relative)
+                        });
+                    }
+                }
+                else if (!string.IsNullOrEmpty(languagesResourcePath))
+                {
+                    var cultureName = selectedItem.Content.ToString();
+                    var culture = new CultureInfo(cultureName == "UA" ? "uk-UA" : "en-US");
+
+                    // Завантаження ресурсів мови
+                    var languageResourcePath = $"{languagesResourcePath}{cultureName}.xaml";
+
+                    var languageResource = new ResourceDictionary
+                    {
+                        Source = new Uri(languageResourcePath, UriKind.RelativeOrAbsolute)
+                    };
+                    resources.MergedDictionaries.Add(languageResource);
+                }
+            }
+        }
+    }
+}
